2f8794d4e4180ee5dec4462ddaa804959a752612
Raphael DDL Oliveira <raphael.s.oliveira@bloomingdales.com>
2019-02-22 17:50:49 -0300

    changed linter to lint-staged

--- build-db-json.js
+++ build-db-json.js
@@ -12,133 +12,133 @@ const finishSuccess = chalk.bold.bgGreen;
 const finishError = chalk.bold.bgRed;
 
 const getDirectories = (baseDir) => {
-    return fs
-        .readdirSync(baseDir, {
-            withFileTypes: true,
-        })
-        .filter((f) => f.isDirectory())
-        .map((f) => path.join(baseDir, f.name));
+	return fs
+		.readdirSync(baseDir, {
+			withFileTypes: true,
+		})
+		.filter((f) => f.isDirectory())
+		.map((f) => path.join(baseDir, f.name));
 };
 
 const mkdirSync = function(dirPath) {
-    try {
-        fs.mkdirSync(dirPath);
-    } catch (err) {
-        if (err.code !== 'EEXIST') {
-            throw err;
-        }
-    }
+	try {
+		fs.mkdirSync(dirPath);
+	} catch (err) {
+		if (err.code !== 'EEXIST') {
+			throw err;
+		}
+	}
 };
 
 const buildFolder = async (dir) => {
-    let baseJsonObject = [];
-    const folderToMatch = path.basename(dir);
+	let baseJsonObject = [];
+	const folderToMatch = path.basename(dir);
 
-    return await new Promise((resolve, reject) => {
-        const fileArray = glob.sync(`${dir}/*.json`, {
-            ignore: ['**/node_modules/**', '**/package.json', '**/package-lock.json', '**/_HERO_TEMPLATE.json'],
-        });
+	return await new Promise((resolve, reject) => {
+		const fileArray = glob.sync(`${dir}/*.json`, {
+			ignore: ['**/node_modules/**', '**/package.json', '**/package-lock.json', '**/_HERO_TEMPLATE.json'],
+		});
 
-        if (!fileArray || (fileArray && !fileArray.length)) {
-            log(warning(`χ No JSON files found.`));
-            return reject();
-        }
-        log(
-            warning(
-                `BUILD: ${
-                    fileArray.length
-                } JSON files were found in "${dir}" folder and will be built into a single file "dist/${folderToMatch}.json".`
-            )
-        );
+		if (!fileArray || (fileArray && !fileArray.length)) {
+			log(warning(`χ No JSON files found.`));
+			return reject();
+		}
+		log(
+			warning(
+				`BUILD: ${
+					fileArray.length
+				} JSON files were found in "${dir}" folder and will be built into a single file "dist/${folderToMatch}.json".`
+			)
+		);
 
-        baseJsonObject = [];
+		baseJsonObject = [];
 
-        // https://stackoverflow.com/questions/31413749/node-js-promise-all-and-foreach
-        let actions = fileArray.map((filename) => {
-            return jsonfile
-                .readFile(filename, {
-                    throws: false,
-                })
-                .then((filecontents = null) => {
-                    if (filecontents === null) {
-                        return Promise.reject(new Error('filecontents null'));
-                    }
-                    let identifierId = filename.replace(/(.*\/)(.*)(\.json)/i, '$2');
-                    filecontents.fileId = identifierId;
-                    filecontents._id = identifierId;
-                    baseJsonObject.push(filecontents);
+		// https://stackoverflow.com/questions/31413749/node-js-promise-all-and-foreach
+		let actions = fileArray.map((filename) => {
+			return jsonfile
+				.readFile(filename, {
+					throws: false,
+				})
+				.then((filecontents = null) => {
+					if (filecontents === null) {
+						return Promise.reject(new Error('filecontents null'));
+					}
+					let identifierId = filename.replace(/(.*\/)(.*)(\.json)/i, '$2');
+					filecontents.fileId = identifierId;
+					filecontents._id = identifierId;
+					baseJsonObject.push(filecontents);
 
-                    return log(success(`√ JSON "${filename}" processed.`));
-                })
-                .catch((err = {}) => {
-                    switch (err.message) {
-                        case 'filecontents null':
-                            log(error(`χ JSON "${filename}" is not valid`));
-                            break;
-                        default:
-                            log(error(`χ An error happened. Please see the following error stack:\,${e.message}`));
-                            break;
-                    }
-                    reject();
-                });
-        });
+					return log(success(`√ JSON "${filename}" processed.`));
+				})
+				.catch((err = {}) => {
+					switch (err.message) {
+						case 'filecontents null':
+							log(error(`χ JSON "${filename}" is not valid`));
+							break;
+						default:
+							log(error(`χ An error happened. Please see the following error stack:\,${e.message}`));
+							break;
+					}
+					reject();
+				});
+		});
 
-        return Promise.all(actions)
-            .then(() => {
-                log(
-                    finishSuccess(
-                        `\n√ All JSON files on "${dir}" folder were processed. See file at "dist/${folderToMatch}.json"\n`
-                    )
-                );
+		return Promise.all(actions)
+			.then(() => {
+				log(
+					finishSuccess(
+						`\n√ All JSON files on "${dir}" folder were processed. See file at "dist/${folderToMatch}.json"\n`
+					)
+				);
 
-                return fs.writeFile(
-                    path.resolve('./dist') + `/${folderToMatch}.json`,
-                    JSON.stringify(baseJsonObject),
-                    (err) => {
-                        if (err) {
-                            log(error(`χ Error writing to dist/"${filename}".json`, err));
-                            return reject();
-                        }
-                        return resolve();
-                    }
-                );
-            })
-            .catch((e) => {
-                log(
-                    finishError(
-                        `\nχ One or more JSONs on "${dir}" folder are not valid. Please fix above file errors and commit the changes.\n`
-                    )
-                );
-                return reject();
-            });
-    });
+				return fs.writeFile(
+					path.resolve('./dist') + `/${folderToMatch}.json`,
+					JSON.stringify(baseJsonObject),
+					(err) => {
+						if (err) {
+							log(error(`χ Error writing to dist/"${filename}".json`, err));
+							return reject();
+						}
+						return resolve();
+					}
+				);
+			})
+			.catch((e) => {
+				log(
+					finishError(
+						`\nχ One or more JSONs on "${dir}" folder are not valid. Please fix above file errors and commit the changes.\n`
+					)
+				);
+				return reject();
+			});
+	});
 };
 
 const buildDb = async () => {
-    //make `dist` folder
-    mkdirSync(path.resolve('./dist'));
+	//make `dist` folder
+	mkdirSync(path.resolve('./dist'));
 
-    const srcDirs = getDirectories('./src');
-    return Promise.all(srcDirs.map((dir) => buildFolder(dir)))
-        .then(() => {
-            log(
-                finishSuccess(
-                    `\n√√√√√√√√√√√√√√√√√√√√√√√√√√√√√√√√√√√√√√√√√√√√√√√√√√√√√√√√√√√\n√ All JSON files of all folders were Built. Congrats!     √\n√√√√√√√√√√√√√√√√√√√√√√√√√√√√√√√√√√√√√√√√√√√√√√√√√√√√√√√√√√√\n`
-                )
-            );
-            return process.exit(0);
-        })
-        .catch((e = {}) => {
-            console.log(e);
-            console.log(e.message);
+	const srcDirs = getDirectories('./src');
+	return Promise.all(srcDirs.map((dir) => buildFolder(dir)))
+		.then(() => {
+			log(
+				finishSuccess(
+					`\n√√√√√√√√√√√√√√√√√√√√√√√√√√√√√√√√√√√√√√√√√√√√√√√√√√√√√√√√√√√\n√ All JSON files of all folders were Built. Congrats!     √\n√√√√√√√√√√√√√√√√√√√√√√√√√√√√√√√√√√√√√√√√√√√√√√√√√√√√√√√√√√√\n`
+				)
+			);
+			return process.exit(0);
+		})
+		.catch((e = {}) => {
+			console.log(e);
+			console.log(e.message);
 
-            log(
-                finishError(
-                    `\nχχχχχχχχχχχχχχχχχχχχχχχχχχχχχχχχχχχχχχχχχχχχχχχχχχ\nχ One or more JSONs are not valid.               χ\nχ Please run "npm test" to discover which        χ\nχ JSON file is the culprit, fix and commit it.   χ\nχχχχχχχχχχχχχχχχχχχχχχχχχχχχχχχχχχχχχχχχχχχχχχχχχχ\n`
-                )
-            );
-            return process.exit(1);
-        });
+			log(
+				finishError(
+					`\nχχχχχχχχχχχχχχχχχχχχχχχχχχχχχχχχχχχχχχχχχχχχχχχχχχ\nχ One or more JSONs are not valid.               χ\nχ Please run "npm test" to discover which        χ\nχ JSON file is the culprit, fix and commit it.   χ\nχχχχχχχχχχχχχχχχχχχχχχχχχχχχχχχχχχχχχχχχχχχχχχχχχχ\n`
+				)
+			);
+			return process.exit(1);
+		});
 };
 
 buildDb();
--- validate.js
+++ validate.js
@@ -13,111 +13,111 @@ const finishSuccess = chalk.bold.bgGreen;
 const finishError = chalk.bold.bgRed;
 
 const getDirectories = (baseDir) => {
-    return fs
-        .readdirSync(baseDir, {
-            withFileTypes: true,
-        })
-        .filter((f) => f.isDirectory())
-        .map((f) => path.join(baseDir, f.name));
+	return fs
+		.readdirSync(baseDir, {
+			withFileTypes: true,
+		})
+		.filter((f) => f.isDirectory())
+		.map((f) => path.join(baseDir, f.name));
 };
 
 const validateFolder = async (dir) => {
-    const folderToMatch = path.basename(dir);
-    return await new Promise((resolve, reject) => {
-        const requiredProperties = validationRules[folderToMatch];
-        const fileArray = glob.sync(`${dir}/*.json`, {
-            ignore: ['**/node_modules/**', '**/package.json', '**/package-lock.json'],
-        });
+	const folderToMatch = path.basename(dir);
+	return await new Promise((resolve, reject) => {
+		const requiredProperties = validationRules[folderToMatch];
+		const fileArray = glob.sync(`${dir}/*.json`, {
+			ignore: ['**/node_modules/**', '**/package.json', '**/package-lock.json'],
+		});
 
-        if (!fileArray || (fileArray && !fileArray.length)) {
-            log(warning(`χ No JSON files found.`));
-            return reject();
-        }
-        log(warning(`VALIDATOR: ${fileArray.length} JSON files were found in "${dir}" folder and will be validated.`));
+		if (!fileArray || (fileArray && !fileArray.length)) {
+			log(warning(`χ No JSON files found.`));
+			return reject();
+		}
+		log(warning(`VALIDATOR: ${fileArray.length} JSON files were found in "${dir}" folder and will be validated.`));
 
-        // https://stackoverflow.com/questions/31413749/node-js-promise-all-and-foreach
-        let actions = fileArray.map((filename) => {
-            return jsonfile
-                .readFile(filename, {
-                    throws: false,
-                })
-                .then((filecontents = null) => {
-                    if (filecontents === null) {
-                        return Promise.reject(new Error('filecontents null'));
-                    }
+		// https://stackoverflow.com/questions/31413749/node-js-promise-all-and-foreach
+		let actions = fileArray.map((filename) => {
+			return jsonfile
+				.readFile(filename, {
+					throws: false,
+				})
+				.then((filecontents = null) => {
+					if (filecontents === null) {
+						return Promise.reject(new Error('filecontents null'));
+					}
 
-                    // TODO: WRITE TEST FOR INNER PROPS, NOT ONLY ROOT ONES
-                    if (
-                        requiredProperties.every((prop) => {
-                            let propTest = prop in filecontents;
+					// TODO: WRITE TEST FOR INNER PROPS, NOT ONLY ROOT ONES
+					if (
+						requiredProperties.every((prop) => {
+							let propTest = prop in filecontents;
 
-                            if (!propTest) {
-                                log(error(`χ JSON "${filename}" does not containt required property "${prop}"!!`));
-                            }
-                            return propTest;
-                        })
-                    ) {
-                        return log(success(`√ JSON "${filename}" is valid.`));
-                    }
-                    return Promise.reject(new Error('missing props'));
-                })
-                .catch((err = {}) => {
-                    switch (err.message) {
-                        case 'filecontents null':
-                            log(error(`χ JSON "${filename}" is not valid`));
-                            break;
-                        case 'missing props':
-                            log(
-                                error(
-                                    `χ JSON ${filename} is missing one or more of the required properties.\nConfirm that the file contains all the following properties:\n    - ${requiredProperties.join(
-                                        `\n    - `
-                                    )}`
-                                )
-                            );
-                            break;
+							if (!propTest) {
+								log(error(`χ JSON "${filename}" does not containt required property "${prop}"!!`));
+							}
+							return propTest;
+						})
+					) {
+						return log(success(`√ JSON "${filename}" is valid.`));
+					}
+					return Promise.reject(new Error('missing props'));
+				})
+				.catch((err = {}) => {
+					switch (err.message) {
+						case 'filecontents null':
+							log(error(`χ JSON "${filename}" is not valid`));
+							break;
+						case 'missing props':
+							log(
+								error(
+									`χ JSON ${filename} is missing one or more of the required properties.\nConfirm that the file contains all the following properties:\n    - ${requiredProperties.join(
+										`\n    - `
+									)}`
+								)
+							);
+							break;
 
-                        default:
-                            log(error(`χ An error happened. Please see the following error stack:\,${e.message}`));
-                            break;
-                    }
-                    reject();
-                });
-        });
+						default:
+							log(error(`χ An error happened. Please see the following error stack:\,${e.message}`));
+							break;
+					}
+					reject();
+				});
+		});
 
-        return Promise.all(actions)
-            .then(() => {
-                log(finishSuccess(`\n√ All JSON files on "${dir}" folder were validated.\n`));
-                return resolve();
-            })
-            .catch((e) => {
-                log(
-                    finishError(
-                        `\nχ One or more JSONs on "${dir}" folder are not valid. Please fix above file errors and commit the changes.\n`
-                    )
-                );
-                return reject();
-            });
-    });
+		return Promise.all(actions)
+			.then(() => {
+				log(finishSuccess(`\n√ All JSON files on "${dir}" folder were validated.\n`));
+				return resolve();
+			})
+			.catch((e) => {
+				log(
+					finishError(
+						`\nχ One or more JSONs on "${dir}" folder are not valid. Please fix above file errors and commit the changes.\n`
+					)
+				);
+				return reject();
+			});
+	});
 };
 
 const validate = async () => {
-    return Promise.all(getDirectories('./src').map((dir) => validateFolder(dir)))
-        .then(() => {
-            log(
-                finishSuccess(
-                    `\n√√√√√√√√√√√√√√√√√√√√√√√√√√√√√√√√√√√√√√√√√√√√√√√√√√√√√√√√√√√\n√ All JSON files of all folders were validated. Congrats! √\n√√√√√√√√√√√√√√√√√√√√√√√√√√√√√√√√√√√√√√√√√√√√√√√√√√√√√√√√√√√\n`
-                )
-            );
-            return process.exit(0);
-        })
-        .catch((e = {}) => {
-            log(
-                finishError(
-                    `\nχχχχχχχχχχχχχχχχχχχχχχχχχχχχχχχχχχχχχχχχχχχχχχχχχχ\nχ One or more JSONs are not valid.               χ\nχ Please fix above files and commit the changes. χ\nχχχχχχχχχχχχχχχχχχχχχχχχχχχχχχχχχχχχχχχχχχχχχχχχχχ\n`
-                )
-            );
-            return process.exit(1);
-        });
+	return Promise.all(getDirectories('./src').map((dir) => validateFolder(dir)))
+		.then(() => {
+			log(
+				finishSuccess(
+					`\n√√√√√√√√√√√√√√√√√√√√√√√√√√√√√√√√√√√√√√√√√√√√√√√√√√√√√√√√√√√\n√ All JSON files of all folders were validated. Congrats! √\n√√√√√√√√√√√√√√√√√√√√√√√√√√√√√√√√√√√√√√√√√√√√√√√√√√√√√√√√√√√\n`
+				)
+			);
+			return process.exit(0);
+		})
+		.catch((e = {}) => {
+			log(
+				finishError(
+					`\nχχχχχχχχχχχχχχχχχχχχχχχχχχχχχχχχχχχχχχχχχχχχχχχχχχ\nχ One or more JSONs are not valid.               χ\nχ Please fix above files and commit the changes. χ\nχχχχχχχχχχχχχχχχχχχχχχχχχχχχχχχχχχχχχχχχχχχχχχχχχχ\n`
+				)
+			);
+			return process.exit(1);
+		});
 };
 
 validate();
